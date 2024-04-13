7bb84532d26a650bf4bcd8577561e5db3eb026bd
Sourav kumar Pradhan <137888438+baMtyPradhan30@users.noreply.github.com>
2024-04-13 18:09:19 +0000

    variable and datatypes

---
+++ javascript/day1.js
@@ -0,0 +1,29 @@
+// To declare constant in js
+const Id = 2101348099
+let Name = "Sourav kumar Pradhan"
+var domain = "web development"
+department = "CSE"
+
+// Id = 21CSE093 // Id can't be changed bcoz it is a constant //
+
+domain = "Data Science"
+department = "CSEAI"
+
+
+console.log(Id); //for one variable output 
+console.table([Id, Name, domain, department]) // for multiple variable output in a tabular format
+
+"use strict"; // treat  all js code as newer version
+
+// datatypes in JS  
+    // number 
+    // bigint
+    // string
+    // boolean => true/ false
+    // null 
+    // undefined
+    // symbol
+let busno = null
+
+console.log(typeof busno);  // object
+console.log(typeof undefined);  // undefined
